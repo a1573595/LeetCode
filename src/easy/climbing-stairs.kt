3a1874e3e70b2bae23dc9ed726ca32130aff9a34
a1573595 <a1573595@gmail.com>
2019-11-28 09:59:45 +0800

    Climbing-stairs、Valid-anagram

---
+++ src/easy/climbing-stairs.kt
@@ -0,0 +1,42 @@
+package easy
+
+import kotlin.system.measureTimeMillis
+
+// https://leetcode.com/problems/climbing-stairs/
+
+fun main(args: Array<String>) {
+    val costTimeMillis1 = measureTimeMillis {
+        getClimbing(45)
+    }
+
+    val costTimeMillis2 = measureTimeMillis {
+        getClimbing2(45)
+    }
+
+    println(costTimeMillis1)
+    println(costTimeMillis2)
+}
+
+fun getClimbing(stair: Int): Int {
+    return when {
+        stair <= 0 -> 0
+        stair == 1 -> 1
+        stair == 2 -> 2
+        else -> getClimbing(stair-1) + getClimbing(stair-2)
+    }
+}
+
+fun getClimbing2(stair: Int): Int {
+    return if(stair < 1) 0
+    else if(stair == 1) 1
+    else {
+        val dp = IntArray(stair + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for(i in 3..stair) {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+
+        dp[stair]
+    }
+}
