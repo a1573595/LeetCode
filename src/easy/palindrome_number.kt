780963ea8538630ba46be25edfcc2587e1dac286
a1573595 <a1573595@gmail.com>
2019-11-29 13:47:09 +0800

    Two-sum、Palindrome-number

---
+++ src/easy/palindrome_number.kt
@@ -0,0 +1,40 @@
+package easy
+
+// https://leetcode.com/problems/palindrome-number/
+
+fun main(args: Array<String>) {
+    println(isPalindrome2(121))
+    println(isPalindrome2(123321))
+}
+
+fun isPalindrome(x: Int): Boolean {
+    if(x < 0 ) return false    // 小於零
+
+    val intList: ArrayList<Int> = ArrayList()
+    var y = x
+
+    while (y > 0) {
+        intList.add(y%10)
+        y /= 10
+    }
+
+    val intList2: ArrayList<Int> = ArrayList()
+    for(i in 0 until intList.size) {
+        intList2.add(intList[intList.size-i-1])
+    }
+
+    return intList.equals(intList2)
+}
+
+fun isPalindrome2(x: Int): Boolean {
+    if(x < 0 ) return false    // 小於零
+
+    var x2 = x
+    var revertedNumber = 0
+    while (x2 > 0) {
+        revertedNumber = revertedNumber * 10 + x2 % 10
+        x2 /= 10
+    }
+
+    return x==revertedNumber
+}
---
+++ src/easy/two_sum.kt
@@ -0,0 +1,39 @@
+package easy
+
+import java.util.*
+
+// https://leetcode.com/problems/two-sum/
+
+fun main(args: Array<String>) {
+    println(twoSum(intArrayOf(1, 2, 3),5).joinToString())
+
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    var index1: Int
+    var index2: Int
+
+    for(i in nums.indices) {
+        index2 = nums.indexOf(target - nums[i])
+        if(index2 != -1 && index2 != i) {
+            index1 = i
+            return intArrayOf(index1, index2)
+        }
+    }
+
+    return intArrayOf()
+}
+
+fun twoSum2(nums: IntArray, target: Int): IntArray? {
+    val map: MutableMap<Int, Int> = HashMap()
+    for (i in nums.indices) {
+        map[nums[i]] = i
+    }
+    for (i in nums.indices) {
+        val complement = target - nums[i]
+        if (map.containsKey(complement) && map[complement] != i) {
+            return intArrayOf(i, map[complement]!!)
+        }
+    }
+    throw IllegalArgumentException("No two sum solution")
+}
