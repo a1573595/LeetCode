d6d431dd5958b912d895748a6a65159d4b2c446b
a1573595 <a1573595@gmail.com>
2021-05-17 22:03:50 +0800

    Plus one

---
+++ src/easy/plus_one.kt
@@ -0,0 +1,37 @@
+package easy
+
+/**
+ * plus-one
+ * https://leetcode.com/problems/plus-one/
+ */
+
+fun main() {
+    println(plusOne(intArrayOf(1, 2, 3)).contentToString())
+    println(plusOne(intArrayOf(4, 3, 2, 1)).contentToString())
+    println(plusOne(intArrayOf(0)).contentToString())
+    println(plusOne(intArrayOf(9, 9)).contentToString())
+    println(plusOne(intArrayOf(8, 9, 9, 9)).contentToString())
+}
+
+/**
+ * 結尾+1再判斷是否進位
+ */
+fun plusOne(digits: IntArray): IntArray {
+    digits[digits.size - 1] += 1
+
+    for (i in digits.size - 1 downTo 0) {
+        if (digits[i] > 9) {
+            digits[i] = 0
+
+            if (i > 0) {
+                digits[i - 1] += 1
+            } else {
+                return intArrayOf(1) + digits
+            }
+        } else {
+            break
+        }
+    }
+
+    return digits
+}
