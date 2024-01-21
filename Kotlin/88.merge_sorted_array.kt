3ce9987e87330a81a62ffe1971bccbea21850e6f
a1573595 <a1573595@gmail.com>
2024-01-21 19:58:19 +0800

    88. Merge Sorted Array

---
+++ Kotlin/88.merge_sorted_array.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println(merge(intArrayOf(1,2,3,0,0,0), 3, intArrayOf(2,5,6), 3))
+    println(merge(intArrayOf(1,), 1, intArrayOf(), 0))
+    println(merge(intArrayOf(0), 0, intArrayOf(1), 1))
+}
+
+fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+    val list = mutableListOf<Int>();
+
+    for(i in 0 ..m-1) {
+        list.add(nums1[i])
+    }
+
+    for(i in 0 .. n-1) {
+        if(list.size >= nums1.size) {
+            break;
+        }
+
+        list.add(nums2[i])
+    }
+
+    list.sort()
+    list.forEachIndexed { index,value-> 
+        nums1[index] = value
+    }
+}
